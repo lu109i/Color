069563b97f9f24f9ddcc2043f0e9e357b3fdb86f
lulu <weidaily303@gmail.com>
2019-02-08 09:52:53 +0800

    color

--- Color/ViewController.swift
+++ Color/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monsterImage: UIImageView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func colorSliderChange(_ sender: Any) {
+        monsterImage.backgroundColor = UIColor(red: CGFloat(redSlider.value)
+            , green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+    }
+    
+    
 
 }
 
